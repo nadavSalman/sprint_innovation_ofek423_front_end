31fcd4fbd72b7c51fbfd796924abe057987835af
hadasMoshe <hadas3355@gmail.com>
2020-12-23 09:46:41 +0200

    create ui for new group model need to complete the logic of colores the chosen users for the new group and need to complete the get/push logic

--- lib/Components/open_page.dart
+++ lib/Components/open_page.dart
@@ -4,16 +4,7 @@ import 'Lists_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-// class OpenPage extends StatefulWidget {
-//   OpenPage();
-
-//   @Override
-
-// }
-
-class OpenPageState extends StatelessWidget {
-  @override
-  // todo: replace the groups list with data from DB
+class OpenPage extends StatefulWidget {
   List<String> groups = [
     "המגניבים",
     "צוות DevOps",
@@ -25,19 +16,29 @@ class OpenPageState extends StatelessWidget {
     "גף תהליכי פיתוח",
   ];
   var _userObject = '';
-  var _lists;
-  var users;
+  var _lists = [];
+  List users = [];
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
-  var newGroupUsers;
-  //add groups parameter to constructor
-  OpenPageState(this._userObject);
+  var _selectedUserIndex;
+  List newGroupUsers = [];
+  var newGroupNameInput = TextEditingController();
+
+  OpenPage(this._userObject);
+
+  @override
+  _OpenPageState createState() => _OpenPageState();
+}
+
+class _OpenPageState extends State<OpenPage> {
+  @override
+  // todo: replace the groups list with data from DB
 
   Widget _buildRow(BuildContext context, int index) {
     //logic code
     if (index.isEven) {
       return ListTile(
         title: Text(
-          groups[index ~/ 2]
+          widget.groups[index ~/ 2]
               .toUpperCase(), //take the string from groups in index of index/2 in integer
           textDirection: TextDirection.rtl,
         ),
@@ -46,8 +47,8 @@ class OpenPageState extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) =>
-                    ListsPage(groups, this._userObject, groups[index ~/ 2])),
+                builder: (context) => ListsPage(widget.groups,
+                    widget._userObject, widget.groups[index ~/ 2])),
           );
         },
       );
@@ -56,53 +57,95 @@ class OpenPageState extends StatelessWidget {
     }
   }
 
+  _addUserToGroup(users, userIndex) {
+    setState(() {
+      widget.newGroupUsers = users;
+      widget._selectedUserIndex = userIndex;
+    });
+  }
+
   Widget _buildOptions() {
     return ListView.builder(
       padding: EdgeInsets.all(16),
       itemBuilder: _buildRow,
-      itemCount: groups.length * 2,
+      itemCount: widget.groups.length * 2,
     );
   }
 
   Widget setupAlertDialoadContainer() {
     return Container(
-      height: 300.0, // Change as per your requirement
-      width: 300.0, // Change as per your requirement
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: users.length * 2,
-        itemBuilder: (BuildContext context, int index) {
-          //logic code
-          if (index.isEven) {
-            return ListTile(
-              title: Text(
-                users[index ~/ 2]["userfullname"]
-                    .toUpperCase(), //take the string from groups in index of index/2 in integer
-                textDirection: TextDirection.rtl,
+        height: 300.0, // Change as per your requirement
+        width: 300.0, // Change as per your requirement
+        child: new Column(
+          children: <Widget>[
+            TextField(
+              controller: widget.newGroupNameInput,
+              textInputAction: TextInputAction.go,
+              decoration: InputDecoration(hintText: "הכנס שם קבוצה חדשה"),
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.right,
+            ),
+            Text(
+              "אנשי קשר להוספה:",
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                fontSize: 20,
               ),
-              onTap: () {
-                newGroupUsers.add(users[index ~/ 2]["userfullname"]);
-              },
-            );
-          } else {
-            return Divider();
-          }
-        },
-      ),
-    );
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.right,
+            ),
+            new Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.users.length * 2,
+                itemBuilder: (BuildContext context, int index) {
+                  //logic code
+                  if (index.isEven) {
+                    return ListTile(
+                      selectedTileColor: Colors.blue,
+                      selected: index == widget._selectedUserIndex,
+                      title: Text(
+                        widget.users[index ~/ 2]["userfullname"]
+                            .toUpperCase(), //take the string from groups in index of index/2 in integer
+                        textDirection: TextDirection.rtl,
+                        textAlign: TextAlign.right,
+                      ),
+                      onTap: () {
+                        List allUsers = widget.newGroupUsers;
+                        allUsers.add(widget.users[index ~/ 2]["userfullname"]);
+                        _addUserToGroup(allUsers, index ~/ 2);
+                      },
+                    );
+                  } else {
+                    return Divider();
+                  }
+                },
+              ),
+            )
+          ],
+        ));
   }
 
   void _showDialog() {
     // flutter defined function
     showDialog(
-      context: _scaffoldKey.currentContext,
+      context: widget._scaffoldKey.currentContext,
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Alert Dialog title"),
+          title: new Text("קבוצה חדשה",
+              textAlign: TextAlign.right,
+              style: TextStyle(decoration: TextDecoration.underline)),
           content: setupAlertDialoadContainer(),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("create"),
+              onPressed: () {
+                // create group' add to local list and post request
+                Navigator.of(context).pop();
+              },
+            ),
             new FlatButton(
               child: new Text("Close"),
               onPressed: () {
@@ -118,7 +161,7 @@ class OpenPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
+      key: widget._scaffoldKey,
       appBar: AppBar(
         title: Text(
           'Meקונה',
@@ -138,8 +181,8 @@ class OpenPageState extends StatelessWidget {
             onPressed: () async {
               var result =
                   await http.get("https://me-kone.herokuapp.com/users");
-              users = jsonDecode(result.body);
-              print(users);
+              widget.users = jsonDecode(result.body);
+              print(widget.users);
 
               _showDialog();
             },
