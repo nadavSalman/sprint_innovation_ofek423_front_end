6a964327c75469b5419f73a74f5e8c64360a48ad
hadasMoshe <hadas3355@gmail.com>
2020-12-24 17:57:16 +0200

    needs to fix post request

--- lib/Components/open_page.dart
+++ lib/Components/open_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'Lists_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'New_group_model.dart';
 
 class OpenPage extends StatefulWidget {
   List<String> groups = [
@@ -19,9 +20,6 @@ class OpenPage extends StatefulWidget {
   var _lists = [];
   List users = [];
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
-  var _selectedUserIndex;
-  List newGroupUsers = [];
-  var newGroupNameInput = TextEditingController();
 
   OpenPage(this._userObject);
 
@@ -57,73 +55,12 @@ class _OpenPageState extends State<OpenPage> {
     }
   }
 
-  _addUserToGroup(users, userIndex) {
-    setState(() {
-      widget.newGroupUsers = users;
-      widget._selectedUserIndex = userIndex;
-    });
-  }
-
   Widget _buildOptions() {
     return ListView.builder(
       padding: EdgeInsets.all(16),
       itemBuilder: _buildRow,
       itemCount: widget.groups.length * 2,
     );
-  }
-
-  Widget setupAlertDialoadContainer() {
-    return Container(
-        height: 300.0, // Change as per your requirement
-        width: 300.0, // Change as per your requirement
-        child: new Column(
-          children: <Widget>[
-            TextField(
-              controller: widget.newGroupNameInput,
-              textInputAction: TextInputAction.go,
-              decoration: InputDecoration(hintText: "הכנס שם קבוצה חדשה"),
-              textDirection: TextDirection.rtl,
-              textAlign: TextAlign.right,
-            ),
-            Text(
-              "אנשי קשר להוספה:",
-              style: TextStyle(
-                decoration: TextDecoration.underline,
-                fontSize: 20,
-              ),
-              textDirection: TextDirection.rtl,
-              textAlign: TextAlign.right,
-            ),
-            new Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: widget.users.length * 2,
-                itemBuilder: (BuildContext context, int index) {
-                  //logic code
-                  if (index.isEven) {
-                    return ListTile(
-                      selectedTileColor: Colors.blue,
-                      selected: index == widget._selectedUserIndex,
-                      title: Text(
-                        widget.users[index ~/ 2]["userfullname"]
-                            .toUpperCase(), //take the string from groups in index of index/2 in integer
-                        textDirection: TextDirection.rtl,
-                        textAlign: TextAlign.right,
-                      ),
-                      onTap: () {
-                        List allUsers = widget.newGroupUsers;
-                        allUsers.add(widget.users[index ~/ 2]["userfullname"]);
-                        _addUserToGroup(allUsers, index ~/ 2);
-                      },
-                    );
-                  } else {
-                    return Divider();
-                  }
-                },
-              ),
-            )
-          ],
-        ));
   }
 
   void _showDialog() {
@@ -136,23 +73,7 @@ class _OpenPageState extends State<OpenPage> {
           title: new Text("קבוצה חדשה",
               textAlign: TextAlign.right,
               style: TextStyle(decoration: TextDecoration.underline)),
-          content: setupAlertDialoadContainer(),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("create"),
-              onPressed: () {
-                // create group' add to local list and post request
-                Navigator.of(context).pop();
-              },
-            ),
-            new FlatButton(
-              child: new Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+          content: NewGroupModel(widget.users),
         );
       },
     );
@@ -183,7 +104,6 @@ class _OpenPageState extends State<OpenPage> {
                   await http.get("https://me-kone.herokuapp.com/users");
               widget.users = jsonDecode(result.body);
               print(widget.users);
-
               _showDialog();
             },
             child: Icon(Icons.add),
