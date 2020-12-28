8cebfa890b184ea62756f6dccbde7b6ad32f0eda
nadavSalman <nadavops@gmail.com>
2020-12-28 20:25:44 +0200

    Update loginUtil.dart

--- lib/logic/loginUtil.dart
+++ lib/logic/loginUtil.dart
@@ -2,6 +2,8 @@
  * Check if a given String contain only numbers.
  */
 bool checkUserPhoneInput(userPhoneFild){
+  if(userPhoneFild.length > 10)
+    return false;
   if (userPhoneFild == null) {
     return false;
   }
